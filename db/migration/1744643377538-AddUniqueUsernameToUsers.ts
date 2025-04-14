2e014350d16982be3940d105e74ad2d8fcf488a1
xuwanwan <1054749044@qq.com>
2025-04-14 23:47:50 +0800

    数据库层面做唯一性校验

---
+++ db/migration/1744643377538-AddUniqueUsernameToUsers.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner, TableIndex } from "typeorm";
+
+export class AddUniqueUsernameToUsers1744643377538 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createIndex('users', new TableIndex({
+            name: 'users_username', columnNames: ['username'], isUnique: true
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropIndex('users', 'users_username');
+    }
+
+}
