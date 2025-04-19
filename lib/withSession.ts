5388c7d95676d764ebb24ef1533db2b726599f58
xuwanwan <1054749044@qq.com>
2025-04-20 02:50:39 +0800

    使用 .env.local 设置环境变量

--- lib/withSession.ts
+++ lib/withSession.ts
@@ -3,7 +3,7 @@ import { withIronSession } from "next-iron-session";
 
 export function withSession(handle: NextApiHandler) {
   return withIronSession(handle, {
-    password: 'b07d63db-11cc-429f-9349-d1fb43c5c7c8',
+    password: process.env.SECRET as string,
     cookieName: 'blog',
     cookieOptions: {
       secure: process.env.NODE_ENV === 'production' ? true : false
