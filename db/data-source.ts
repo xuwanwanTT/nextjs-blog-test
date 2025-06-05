4a266c3db325f2a793b13e4f621c93df70d6c60e
xuwanwan <1054749044@qq.com>
2025-06-06 07:50:48 +0800

    docker化

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "192.168.1.13",
     port: 5432,
     username: "blog",
     password: "",
