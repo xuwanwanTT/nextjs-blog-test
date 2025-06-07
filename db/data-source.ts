56458e81fcf4db9e93e524c17ada524e0539dc1e
xuwanwan <1054749044@qq.com>
2025-06-07 14:55:06 +0800

    update orm config

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,10 +1,23 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import config from "../ormconfig.json";
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
 
 export const AppDataSource = new DataSource({
-    ...config as PostgresConnectionOptions,
-    host: process.env.NODE_ENV === "production" ? "localhost" : config.host,
-    database: process.env.NODE_ENV === "production" ? "blog_production" : "blog_development"
+    "type": "postgres",
+    host: process.env.NODE_ENV === "production" ? "localhost" : "192.168.1.13",
+    "port": 5432,
+    "username": "blog",
+    "password": "",
+    database: process.env.NODE_ENV === "production" ? "blog_production" : "blog_development",
+    "synchronize": false,
+    "logging": false,
+    "entities": [
+        "dist/entity/**/*.js"
+    ],
+    "migrations": [
+        "dist/migration/**/*.js"
+    ],
+    "subscribers": [
+        "dist/subscribers/**/*.js"
+    ],
+    "migrationsRun": true
 });
--- dist/data-source.js
+++ dist/data-source.js
@@ -1,17 +1,22 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.AppDataSource = void 0;
-var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
 require("reflect-metadata");
 var _typeorm = require("typeorm");
-var _ormconfig = _interopRequireDefault(require("../ormconfig.json"));
-function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
-function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { (0, _defineProperty2["default"])(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
-var AppDataSource = exports.AppDataSource = new _typeorm.DataSource(_objectSpread(_objectSpread({}, _ormconfig["default"]), {}, {
-  host: process.env.NODE_ENV === "production" ? "localhost" : _ormconfig["default"].host,
-  database: process.env.NODE_ENV === "production" ? "blog_production" : "blog_development"
-}));
+var AppDataSource = exports.AppDataSource = new _typeorm.DataSource({
+  "type": "postgres",
+  host: process.env.NODE_ENV === "production" ? "localhost" : "192.168.1.13",
+  "port": 5432,
+  "username": "blog",
+  "password": "",
+  database: process.env.NODE_ENV === "production" ? "blog_production" : "blog_development",
+  "synchronize": false,
+  "logging": false,
+  "entities": ["dist/entity/**/*.js"],
+  "migrations": ["dist/migration/**/*.js"],
+  "subscribers": ["dist/subscribers/**/*.js"],
+  "migrationsRun": true
+});
